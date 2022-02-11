8865c96c5f2d1bc26e2e775a61499bd49a8c2a81
Andrew <andrew.afony@yandex.ru>
2022-02-11 18:23:33 +0300

    add AddEditNoteScreen

---
+++ app/src/main/java/com/myapp/noteapp/presentation/Screen.kt
@@ -0,0 +1,6 @@
+package com.myapp.noteapp.presentation
+
+sealed class Screen (val route: String) {
+    object NotesScreen: Screen("notes_screen")
+    object AddEditNoteScreen: Screen("add_edit_note_screen")
+}
